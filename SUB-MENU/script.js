ac33406bf2123c0e8c7b6d7965e27740d2d2402f
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-05-25 00:52:53 +0900

    드롭 다운 하위 메뉴 ver. 2
    단순히 하위 메뉴의 height를 270px로 설정하는 것이 아닌, subMenu의 height와 header의 height에 따른 하위 메뉴의 height 설정함

--- SUB-MENU/script.js
+++ SUB-MENU/script.js
@@ -1,18 +1,40 @@
 var header = document.querySelector("header"),
-  nav = document.querySelector("nav");
+  mainMenuList = document.querySelectorAll(".mainmenu > li"),
+  subMenu = document.querySelectorAll(".submenu"),
+  // querySelectorAll 로 불러오면, subMenu라는 배열로 요소들 저장됨
+  headerHeight = header.offsetHeight,
+  subMenuHeight = 0;
+
+/*
+    var B = A.offsetHeight; // border까지의 높이 (50)
+    var B = A.clientHeight; // padding까지의 높이 (50)
+
+    변수명 subMenuHeight에 subMenu 중에서 가장 높이가 큰 요소의 높이 구해서 저장.
+    특정 하위 메뉴의 높이가 늘어나도, js 코드 수정할 필요 없음!
+*/
+
+// subMenu 배열의 길이만큼 순회
+for (var i = 0; i < subMenu.length; i++) {
+  if (subMenu[i].offsetHeight > subMenuHeight) {
+    // i번째 요소의 height가 subMenuHeight보다 클 때
+    subMenuHeight = subMenu[i].offsetHeight;
+  }
+}
+
+/*
+  mainMenuList에 마우스 올리면 header 높이를 headerHeight와 subMenuHeight를 더한 최종 크기로 변경
+  mainMenuList에 마우스 벗어나면 headerHeight로 변경
+*/
 
 function openSubMenu() {
-  header.style.height = "270px";
+  header.style.height = headerHeight + subMenuHeight + "px";
 }
 
 function closeSubMenu() {
-  header.style.height = "50px";
+  header.style.height = headerHeight + "px";
 }
 
-nav.addEventListener("mouseover", openSubMenu);
-nav.addEventListener("mouseout", closeSubMenu);
-
-/*
-        nav에 마우스 올리면 header 높이가 270px 로,
-        마우스 나가면 header 높이를 50px로 변경
-    */
+for (var i = 0; i < mainMenuList.length; i++) {
+  mainMenuList[i].addEventListener("mouseover", openSubMenu);
+  mainMenuList[i].addEventListener("mouseout", closeSubMenu);
+}
