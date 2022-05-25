832896c106172a2bb1bf6d661ff78864a85f2fa4
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-05-25 14:51:29 +0900

    드롭 다운 하위 메뉴 ver. 3
    mainmenu의 하위 요소인 4개의 li에 각각 마우스 hover할 때마다, 그 li의 하위 요소인 ul의 height 값에 따라 적절한 하위 메뉴의 height값을 설정함

--- SUB-MENU/script.js
+++ SUB-MENU/script.js
@@ -6,27 +6,12 @@ var header = document.querySelector("header"),
   subMenuHeight = 0;
 
 /*
-    var B = A.offsetHeight; // border까지의 높이 (50)
-    var B = A.clientHeight; // padding까지의 높이 (50)
-
-    변수명 subMenuHeight에 subMenu 중에서 가장 높이가 큰 요소의 높이 구해서 저장.
-    특정 하위 메뉴의 높이가 늘어나도, js 코드 수정할 필요 없음!
-*/
-
-// subMenu 배열의 길이만큼 순회
-for (var i = 0; i < subMenu.length; i++) {
-  if (subMenu[i].offsetHeight > subMenuHeight) {
-    // i번째 요소의 height가 subMenuHeight보다 클 때
-    subMenuHeight = subMenu[i].offsetHeight;
-  }
-}
-
-/*
   mainMenuList에 마우스 올리면 header 높이를 headerHeight와 subMenuHeight를 더한 최종 크기로 변경
   mainMenuList에 마우스 벗어나면 headerHeight로 변경
 */
 
 function openSubMenu() {
+  subMenuHeight = this.querySelector("ul").offsetHeight; // this := mainMenuList[i], ul := subMenu[i]
   header.style.height = headerHeight + subMenuHeight + "px";
 }
 
@@ -35,6 +20,11 @@ function closeSubMenu() {
 }
 
 for (var i = 0; i < mainMenuList.length; i++) {
+  // subMenuHeight = subMenu[i].offsetHeight;
+  /*
+      마우스가 올라간 그 요소의 자식요소(ul)의 높이를 변수명 subMenuHeight에 저장하고
+      header의 높이를 headerHeight + subMenuHeight로 변경
+  */
   mainMenuList[i].addEventListener("mouseover", openSubMenu);
   mainMenuList[i].addEventListener("mouseout", closeSubMenu);
 }
