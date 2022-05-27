983f61787836084ff5151ec3edbfd1d00e6bcbbb
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-05-27 16:36:30 +0900

    스크롤 애니메이션 ver .2
    ver .1 에서 2,3번째 <section> 태그 내 <div> 요소들이 화면 상에 나타나는 타이밍이 동일하지 않은 점을 개선하기 위한 코드. 각 section 마다 표시 타이밍의 기준이 되는 element 한 개 정해서 그 element 타이밍에 모든 section 내 element가 맞추도록 함. => 기준이 되는 요소에 "id 속성" 추가하고, 기준이 아닌 요소에는 "data-sa-trigger" 속성 추가 후, 그 속성의 값에 기준 요소의 id 값 지정함.

--- SCROLL-ANIMATION-EASY/script.js
+++ SCROLL-ANIMATION-EASY/script.js
@@ -1,4 +1,5 @@
 const saTriggerMargin = 300; // 특정 element가 화면 중앙에 왔을 때의 위치 구하기 위한 변수
+let saTriggerHeight = 0; // data-sa-trigger 속성 가진 디비전과, 속성 없는 디비전 기준으로 요소 나타나는 위치 정하기 위한 변수
 const saElementList = document.querySelectorAll(".sa");
 
 // fucntion saFunc() { ... }
@@ -7,11 +8,21 @@ const saFunc = function () {
   for (const element of saElementList) {
     if (!element.classList.contains("show")) {
       // 특정 요소가 보여지지 않았을 때
-      if (
-        // 화면 상 보이는 높이 (body 전체 높이 x)
-        window.innerHeight >
-        element.getBoundingClientRect().top + saTriggerMargin // 특정 요소 위치의 top 값 + 300
-      ) {
+      if (element.dataset.saTrigger) {
+        // (= '#saDiv5' or '#saDiv9') data-sa-trigger 속성 가진 디비전일 경우
+        saTriggerHeight =
+          document
+            .querySelector(element.dataset.saTrigger)
+            .getBoundingClientRect().top + saTriggerMargin;
+        /*  
+          element.dataset.saTrigger = '#saDiv5'이므로 
+          document.querySelector(element.dataset.saTrigger)은 'saDiv5'를 id로 갖는 '디비전 5' div 의미
+        */
+      } else {
+        saTriggerHeight = element.getBoundingClientRect().top + saTriggerMargin; // 특정 요소 위치의 top 값 + 300
+      }
+      // 스크롤 내리다가 화면 상 보이는 높이 (window.innerHeight)가 saTriggerHeight보다 더 커질 때 (= 해당 element가 화면 중앙 쯤 위치했을 때)
+      if (window.innerHeight > saTriggerHeight) {
         element.classList.add("show"); // 특정 요소 보이도록 "show" class 추가
       }
     }
